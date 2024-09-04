a648814f4a8ab1fd35ce2c6107f37d3ad031ab42
Amar Husain <amar.husain156@gmail.com>
2024-09-04 12:56:25 +0530

    modified footer

--- src/app/layout/footer/footer-area/footer-area.component.ts
+++ src/app/layout/footer/footer-area/footer-area.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterCopyrightComponent } from '../footer-copyright/footer-copyright.component';
 import { SupportFooterAreaComponent } from '../support-footer-area/support-footer-area.component';
 
 @Component({
   selector: 'app-footer-area',
   standalone: true,
-  imports: [SupportFooterAreaComponent],
+  imports: [SupportFooterAreaComponent, FooterCopyrightComponent],
   templateUrl: './footer-area.component.html',
   styleUrl: './footer-area.component.scss'
 })
