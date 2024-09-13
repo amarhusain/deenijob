8509b3b2cbc81f79f5f567aa4ed2f5dc5f2f6fc7
Amar Husain <amar.husain156@gmail.com>
2024-09-13 09:57:02 +0530

    test 10

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Deenijob' title`, () => {
+  it(`should have the 'Deenijob' in title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Deenijob');
