ed7eac61ae24022072e42c74024f399fc0b27012
Amar Husain <amar.husain156@gmail.com>
2024-09-13 09:11:35 +0530

    test 8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rudra-frontend' title`, () => {
+  it(`should have the 'Deenijob' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rudra-frontend');
+    expect(app.title).toEqual('Deenijob');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rudra-frontend');
-  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,5 @@ import { HomePageComponent } from './pages/home/home-page/home-page.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  title = "Deenijob"
 }
